0de1c857759dccff97b97bf81a0c306883e4e8cd
Mayank Chandel <chandelmayank580@gmail.com>
2025-08-15 14:50:23 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import chatRoutes from "./routes/chat.routes.js";
 const app = express();
 app.use(
   cors({
-    origin: ["https://persona-chat-drab-tau.vercel.app"],
+    origin: ["http://localhost:5173", "https://persona-drab-tau.vercel.app"],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
